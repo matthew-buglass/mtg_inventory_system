7f4617ea4ec842fce1d00d9649e7f1950cddad34
Matthew Buglass <matthewbuglass@gmail.com>
2023-02-27 16:46:09 -0600

    Set up a CardCypherSerializer

--- src/mtg_inventory_system/neo4j_utils/cypher_serializers.py
+++ src/mtg_inventory_system/neo4j_utils/cypher_serializers.py
@@ -1,21 +1,37 @@
 import django.db.models
 
+from logging import getLogger
+
 from .utils import *
+from ..common.models import Card
+
+logger = getLogger(__name__)
 
 
-class CypherSerializer:
-    def __init__(self):
-        self.create_node_fn = create_node_clause
-        self.create_relation_fn = create_relationship_clause
+class CypherSerializerBase:
+    def __init__(
+            self,
+            model_class,
+            create_node_fn=create_node_clause,
+            create_relation_fn=create_relationship_clause,
+            fields=None
+    ):
+        if fields is None:
+            fields = []
 
-    def create_string(self, obj: django.db.models.Model, fields: dict):
+        self.create_node_fn = create_node_fn
+        self.create_relation_fn = create_relation_fn
+        self.model_class = model_class
+        self.fields = fields
+
+    def get_create_node_string(self, obj: django.db.models.Model, fields: dict):
         return self.create_node_fn(
             node_id=obj.id,
             label=obj.__class__.__name__,
             props=fields
         )
 
-    def create_relation(
+    def get_create_relation_string(
             self,
             src_obj: django.db.models.Model,
             dst_obj: django.db.models.Model,
@@ -30,3 +46,29 @@ class CypherSerializer:
             relation_id=relation_id,
             props=fields
         )
+
+    def get_create_all_nodes_string(self):
+        create_strings_list = []
+        if self.model_class:
+            objects = self.model_class.objects.all()
+            for x in iter(objects):
+                fields = x.values(*self.fields)
+                create_strings_list.append(self.get_create_node_string(
+                    obj=x,
+                    fields=fields
+                ))
+
+        logger.info(f'Got create string for {len(create_strings_list)} {self.model_class.__name__} nodes')
+        return '\n'.join(create_strings_list)
+
+    def write_create_all_nodes_string_to_file(self, file_name):
+        with open(file_name, 'w') as f:
+            f.write(self.get_create_all_nodes_string())
+            f.close()
+
+        logger.info(f'Wrote create {self.model_class.__name__} nodes to cypher file {file_name}')
+
+
+class CardCypherSerializer(CypherSerializerBase):
+    def __init__(self):
+        super(CardCypherSerializer, self).__init__(model_class=Card, fields=['name', 'collector_number'])
