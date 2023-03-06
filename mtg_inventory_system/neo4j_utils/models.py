6e41fb10440d19cb062d5a2bc4989d3d0c0f678e
Matthew Buglass <matthewbuglass@gmail.com>
2023-03-06 13:14:29 -0600

    added a few things to prevent import errors

--- mtg_inventory_system/neo4j_utils/models.py
+++ mtg_inventory_system/neo4j_utils/models.py
@@ -1,10 +1,10 @@
 from django.db.models import Model, JSONField, ForeignKey, CharField
 
-from common.models import Card
+
 
 
 class CardNodeConnection(Model):
-    src_card = ForeignKey(Card)
-    dst_card =
-    connection_type =
+    src_card = None
+    dst_card =None
+    connection_type =None
     connection_meta_data = JSONField(null=True)
